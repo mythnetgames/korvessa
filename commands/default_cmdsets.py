62c8b2aedeccc458044632146bbf61f65d4c19eb
mythnetgames <mythnetgames@gmail.com>
2025-12-31 00:53:49 -0600

    Comment out character and account command additions in default command sets

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -15,18 +15,18 @@ own cmdsets by inheriting from them or directly from `evennia.CmdSet`.
 """
 
 from evennia import default_cmds
-from commands.unlogged_commands import CmdAccountCreate, CmdAccountLogin
-from commands.character_menu import (
-    CmdLoginCharacter,
-    CmdSubmitApplication,
-    CmdDeleteApplication,
-    CmdRetireCharacter,
-    CmdViewCharacters,
-    CmdUpdateEmail,
-    CmdChangePassword,
-    CmdOOCMail,
-    CmdLogout,
-)
+## from commands.unlogged_commands import CmdAccountCreate, CmdAccountLogin
+## from commands.character_menu import (
+##     CmdLoginCharacter,
+##     CmdSubmitApplication,
+##     CmdDeleteApplication,
+##     CmdRetireCharacter,
+##     CmdViewCharacters,
+##     CmdUpdateEmail,
+##     CmdChangePassword,
+##     CmdOOCMail,
+##     CmdLogout,
+## )
 
 
 class CharacterCmdSet(default_cmds.CharacterCmdSet):
@@ -64,15 +64,15 @@ class AccountCmdSet(default_cmds.AccountCmdSet):
         """
         super().at_cmdset_creation()
         # Add character menu commands
-        self.add(CmdLoginCharacter())
-        self.add(CmdSubmitApplication())
-        self.add(CmdDeleteApplication())
-        self.add(CmdRetireCharacter())
-        self.add(CmdViewCharacters())
-        self.add(CmdUpdateEmail())
-        self.add(CmdChangePassword())
-        self.add(CmdOOCMail())
-        self.add(CmdLogout())
+        # self.add(CmdLoginCharacter())
+        # self.add(CmdSubmitApplication())
+        # self.add(CmdDeleteApplication())
+        # self.add(CmdRetireCharacter())
+        # self.add(CmdViewCharacters())
+        # self.add(CmdUpdateEmail())
+        # self.add(CmdChangePassword())
+        # self.add(CmdOOCMail())
+        # self.add(CmdLogout())
 
 
 class UnloggedinCmdSet(default_cmds.UnloggedinCmdSet):
@@ -89,11 +89,11 @@ class UnloggedinCmdSet(default_cmds.UnloggedinCmdSet):
         """
         super().at_cmdset_creation()
         # Remove default create/connect commands to avoid conflicts
-        self.delete("create")
-        self.delete("connect")
+        # self.delete("create")
+        # self.delete("connect")
         # Add custom Korvessa login commands
-        self.add(CmdAccountCreate())
-        self.add(CmdAccountLogin())
+        # self.add(CmdAccountCreate())
+        # self.add(CmdAccountLogin())
 
 
 class SessionCmdSet(default_cmds.SessionCmdSet):
