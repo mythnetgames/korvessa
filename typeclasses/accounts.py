e8fb410691701efb55611f038590e48a26866e2f
mythnetgames <mythnetgames@gmail.com>
2026-01-13 15:46:49 -0600

    Refactor Guest class to streamline post-login behavior and restrict OOC menu access.

--- typeclasses/accounts.py
+++ typeclasses/accounts.py
@@ -82,110 +82,6 @@ class Account(DefaultAccount):
         # Always allow messages through - the cutscene needs to display
         super().msg(text=text, from_obj=from_obj, session=session, options=options, **kwargs)
 
-
-class Guest(DefaultGuest):
-    """
-    Guest account - a simple low-level account for testing.
-    
-    An Account is the actual OOC player entity. It doesn't exist in the game,
-    but puppets characters.
-
-    This is the base Typeclass for all Accounts. Accounts represent
-    the person playing the game and tracks account info, password
-    etc. They are OOC entities without presence in-game. An Account
-    can connect to a Character Object in order to "enter" the
-    game.
-
-    Account Typeclass API:
-
-    * Available properties (only available on initiated typeclass objects)
-
-     - key (string) - name of account
-     - name (string)- wrapper for user.username
-     - aliases (list of strings) - aliases to the object. Will be saved to
-            database as AliasDB entries but returned as strings.
-     - dbref (int, read-only) - unique #id-number. Also "id" can be used.
-     - date_created (string) - time stamp of object creation
-     - permissions (list of strings) - list of permission strings
-     - user (User, read-only) - django User authorization object
-     - obj (Object) - game object controlled by account. 'character' can also
-                     be used.
-     - is_superuser (bool, read-only) - if the connected user is a superuser
-
-    * Handlers
-
-     - locks - lock-handler: use locks.add() to add new lock strings
-     - db - attribute-handler: store/retrieve database attributes on this
-                              self.db.myattr=val, val=self.db.myattr
-     - ndb - non-persistent attribute handler: same as db but does not
-                                  create a database entry when storing data
-     - scripts - script-handler. Add new scripts to object with scripts.add()
-     - cmdset - cmdset-handler. Use cmdset.add() to add new cmdsets to object
-     - nicks - nick-handler. New nicks with nicks.add().
-     - sessions - session-handler. Use session.get() to see all sessions connected, if any
-     - options - option-handler. Defaults are taken from settings.OPTIONS_ACCOUNT_DEFAULT
-     - characters - handler for listing the account's playable characters
-
-    * Helper methods (check autodocs for full updated listing)
-
-     - msg(text=None, from_obj=None, session=None, options=None, **kwargs)
-     - execute_cmd(raw_string)
-     - search(searchdata, return_puppet=False, search_object=False, typeclass=None,
-                      nofound_string=None, multimatch_string=None, use_nicks=True,
-                      quiet=False, **kwargs)
-     - is_typeclass(typeclass, exact=False)
-     - swap_typeclass(new_typeclass, clean_attributes=False, no_default=True)
-     - access(accessing_obj, access_type='read', default=False, no_superuser_bypass=False, **kwargs)
-     - check_permstring(permstring)
-     - get_cmdsets(caller, current, **kwargs)
-     - get_cmdset_providers()
-     - uses_screenreader(session=None)
-     - get_display_name(looker, **kwargs)
-     - get_extra_display_name_info(looker, **kwargs)
-     - disconnect_session_from_account()
-     - puppet_object(session, obj)
-     - unpuppet_object(session)
-     - unpuppet_all()
-     - get_puppet(session)
-     - get_all_puppets()
-     - is_banned(**kwargs)
-     - get_username_validators(validator_config=settings.AUTH_USERNAME_VALIDATORS)
-     - authenticate(username, password, ip="", **kwargs)
-     - normalize_username(username)
-     - validate_username(username)
-     - validate_password(password, account=None)
-     - set_password(password, **kwargs)
-     - get_character_slots()
-     - get_available_character_slots()
-     - create_character(*args, **kwargs)
-
-    * Hook methods:
-
-     - basetype_setup()
-     - at_account_creation()
-     - at_init()
-     - at_first_save()
-     - at_access()
-     - at_cmdset_get(**kwargs)
-     - at_password_change(**kwargs)
-     - at_first_login()
-     - at_pre_login()
-     - at_post_login(session=None)
-     - at_failed_login(session, **kwargs)
-     - at_disconnect(reason=None, **kwargs)
-     - at_post_disconnect(**kwargs)
-     - at_message_receive()
-     - at_message_send()
-     - at_server_reload()
-     - at_server_shutdown()
-     - at_look(target=None, session=None, **kwargs)
-     - at_post_create_character(character, **kwargs)
-     - at_post_add_character(char)
-     - at_post_remove_character(char)
-     - at_pre_channel_msg(message, channel, senders=None, **kwargs)
-     - at_post_chnnel_msg(message, channel, senders=None, **kwargs)
-    """
-
     def check_available_slots(self, **kwargs):
         """
         Override Evennia's default to exclude archived characters from slot count.
@@ -335,6 +231,48 @@ class Guest(DefaultGuest):
     """
     This class is used for guest logins. Unlike Accounts, Guests and their
     characters are deleted after disconnection.
+    
+    Guests are restricted to only character creation or ic <name> to play.
     """
 
-    pass
+    def at_post_login(self, session=None, **kwargs):
+        """
+        Called after successful guest login.
+        
+        Guests only get character creation or auto-puppet - no OOC menu.
+        """
+        # Get playable characters
+        all_characters = self.characters.all()
+        
+        # Filter for active (non-archived) characters
+        active_chars = []
+        for char in all_characters:
+            archived_status = getattr(char.db, 'archived', False)
+            if archived_status is not True:
+                active_chars.append(char)
+        
+        if len(active_chars) == 0:
+            # No characters - start character creation
+            try:
+                from commands.charcreate import start_character_creation
+                start_character_creation(self, is_respawn=False, old_character=None)
+            except ImportError:
+                self.msg("|rCharacter creation system not available. Please contact an admin.|n")
+        elif len(active_chars) == 1:
+            # One character - auto-puppet
+            if session:
+                self.puppet_object(session, active_chars[0])
+            else:
+                self.msg("|yUse |wic|y to enter the game.|n")
+        else:
+            # Multiple characters - tell them to use ic
+            self.msg("|yYou have multiple characters. Use |wic <name>|y to enter the game.|n")
+            for char in active_chars:
+                self.msg(f"  - {char.key}")
+
+    def at_look(self, target=None, session=None, **kwargs):
+        """
+        Block the OOC menu for guests - they should only use ic or chargen.
+        """
+        # Return minimal info instead of full OOC menu
+        return ""
